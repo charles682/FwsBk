18e1b156cecb93fb4cdce4cb5fb8610626a329e4
Charles Melo <melocharles220@gmail.com>
2025-07-11 18:58:50 -0300

    chore: page slug

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import RestaurantPage from "@/app/[slug]/page";
+
+const HomePage = () => {
+  return (
+    <>
+      <RestaurantPage params={undefined} />;
+    </>
+  );
+};
+
+export default HomePage;
