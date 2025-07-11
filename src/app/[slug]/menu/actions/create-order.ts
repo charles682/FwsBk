ccc25ec7f4989cf4080402c20ad415a40237a84d
Charles Melo <melocharles220@gmail.com>
2025-07-11 18:38:01 -0300

    chore: revalidatePath

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -57,7 +58,7 @@ export const createOrder = async (input: createOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  revalidatePath(`/${input.slug}/orders`); 
+  revalidatePath(`/${input.slug}/orders`);
   redirect(
     `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
   ); // redirecionar para a pagina de pedidos ja realizados
